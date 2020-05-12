ed9ab6baf4aba68be570c1d4e14b809b56c7ae5d
Ayoola Olafenwa <olafenwaayoola@gmail.com>
2020-05-12 20:51:15 +0100

    updated

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#import os
-#import sys
-#sys.path.insert(0, os.path.abspath())
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -55,6 +55,7 @@ extensions = [
 
 html_theme = "sphinx_rtd_theme"
 pygments_style = 'sphinx'
+
 #html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
