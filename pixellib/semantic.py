e39cfc7d695a2995fbb8ec36d5ab6b0e8971ed7b
Ayoola Olafenwa <olafenwaayoola@gmail.com>
2020-12-02 11:22:41 +0100

    updated

--- pixellib/semantic.py
+++ pixellib/semantic.py
@@ -9,13 +9,40 @@ from datetime import datetime
 
 
 class semantic_segmentation():
-  def __init__(self):
+  INPUT_TENSOR_NAME = 'ImageTensor:0'
+  OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
+  INPUT_SIZE = 513
+  FROZEN_GRAPH_NAME = 'frozen_inference_graph'
+
+  
+  def __init__(self, model_type = "h5"):
+    global model_file
+    self.model_type = model_type
+    model_file = model_type
+
     self.model = Deeplab_xcep_pascal()
 
     self.model2 = Deeplab_xcep_ade20k()
       
   def load_pascalvoc_model(self, model_path):
-    self.model.load_weights(model_path)
+    if model_file == "pb":
+      self.graph = tf.Graph()
+
+      graph_def = None
+
+      with tf.compat.v1.gfile.GFile(model_path, 'rb')as file_handle:
+        graph_def = tf.compat.v1.GraphDef.FromString(file_handle.read())
+
+      if graph_def is None:
+        raise RuntimeError('Cannot find inference graph')
+
+      with self.graph.as_default():
+        tf.graph_util.import_graph_def(graph_def, name='')
+
+      self.sess = tf.compat.v1.Session(graph=self.graph)
+
+    else:
+      self.model.load_weights(model_path)
     
   def load_ade20k_model(self, model_path):
     self.model2.load_weights(model_path)   
@@ -23,56 +50,39 @@ class semantic_segmentation():
 
 ####### SEMANTIC SEGMENTATION WITH PASCALVOC MODEL ######    
 
-  def segmentAsPascalvoc(self, image_path, output_image_name=None,overlay=False, process_frame = True, verbose = None):            
-    trained_image_width=512
-    mean_subtraction_value=127.5
-    if process_frame == True:
-      image = image_path
-    else:  
-      image = np.array(Image.open(image_path))     
-   
+  def segmentAsPascalvoc(self, image_path, process_frame = False, output_image_name=None,overlay=False,  verbose = None): 
 
-    # resize to max dimension of images from training dataset
-    w, h, n = image.shape
+    if model_file == "pb":
 
-    if n > 3:
-      image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
-    
-    image_overlay = image.copy()
+      if process_frame == True:
+        image = image_path
 
-    ratio = float(trained_image_width) / np.max([w, h])
-    resized_image = np.array(Image.fromarray(image.astype('uint8')).resize((int(ratio * h), int(ratio * w))))
-    resized_image = (resized_image / mean_subtraction_value) -1
+      else:
+        image = cv2.imread(image_path)
 
+      w,h, n = image.shape
 
-    # pad array to square image to match training images
-    pad_x = int(trained_image_width - resized_image.shape[0])
-    pad_y = int(trained_image_width - resized_image.shape[1])
-    resized_image = np.pad(resized_image, ((0, pad_x), (0, pad_y), (0, 0)), mode='constant')
+      image_overlay = image.copy()
+     
+      if n > 3:
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
 
-    if verbose is not None:
-      print("Processing image....")
+      resize_ratio = 1.0 * self.INPUT_SIZE / max(w, h)
+      target_size = (int(resize_ratio * w), int(resize_ratio * h))
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      resized_image = cv2.resize(image, target_size, interpolation=cv2.INTER_AREA)
+      batch_seg_map = self.sess.run(
+        self.OUTPUT_TENSOR_NAME,
+        feed_dict={self.INPUT_TENSOR_NAME: [np.asarray(resized_image)]})
+     
+      seg_image = batch_seg_map[0]
+      raw_labels = seg_image
+      #Apply segmentation color map
+      labels = labelP_to_color_image(seg_image)   
+      labels = np.array(Image.fromarray(labels.astype('uint8')).resize((h, w)))
+      new_img = cv2.cvtColor(labels, cv2.COLOR_RGB2BGR)
 
-    #run prediction
-    res = self.model.predict(np.expand_dims(resized_image, 0))
-    
-    labels = np.argmax(res.squeeze(), -1)
-    # remove padding and resize back to original image
-    if pad_x > 0:
-        labels = labels[:-pad_x]
-    if pad_y > 0:
-        labels = labels[:, :-pad_y]
-
-    raw_labels = labels
-        
-    #Apply segmentation color map
-    labels = labelP_to_color_image(labels)   
-    labels = np.array(Image.fromarray(labels.astype('uint8')).resize((h, w)))
-    
-    
-    new_img = cv2.cvtColor(labels, cv2.COLOR_RGB2BGR)
-
-    if overlay == True:
+      if overlay == True:
         alpha = 0.7
         cv2.addWeighted(new_img, alpha, image_overlay, 1 - alpha,0, image_overlay)
 
@@ -83,7 +93,79 @@ class semantic_segmentation():
         return raw_labels, image_overlay
 
         
-    else:  
+      else:  
+        if output_image_name is not None:
+  
+          cv2.imwrite(output_image_name, new_img)
+
+          print("Processed Image saved successfuly in your current working directory.")
+
+        return raw_labels, new_img 
+
+      
+    else:
+
+      trained_image_width=512
+      mean_subtraction_value=127.5
+
+      if process_frame == True:
+        image = image_path
+      else:  
+        image = np.array(Image.open(image_path))     
+   
+
+      # resize to max dimension of images from training dataset
+      w, h, n = image.shape
+
+      if n > 3:
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
+    
+      image_overlay = image.copy()
+
+      ratio = float(trained_image_width) / np.max([w, h])
+      resized_image = np.array(Image.fromarray(image.astype('uint8')).resize((int(ratio * h), int(ratio * w))))
+      resized_image = (resized_image / mean_subtraction_value) -1
+
+
+      # pad array to square image to match training images
+      pad_x = int(trained_image_width - resized_image.shape[0])
+      pad_y = int(trained_image_width - resized_image.shape[1])
+      resized_image = np.pad(resized_image, ((0, pad_x), (0, pad_y), (0, 0)), mode='constant')
+
+      if verbose is not None:
+        print("Processing image....")
+
+      #run prediction
+      res = self.model.predict(np.expand_dims(resized_image, 0))
+    
+      labels = np.argmax(res.squeeze(), -1)
+      # remove padding and resize back to original image
+      if pad_x > 0:
+        labels = labels[:-pad_x]
+      if pad_y > 0:
+        labels = labels[:, :-pad_y]
+
+      raw_labels = labels
+        
+      #Apply segmentation color map
+      labels = labelP_to_color_image(labels)   
+      labels = np.array(Image.fromarray(labels.astype('uint8')).resize((h, w)))
+    
+    
+      new_img = cv2.cvtColor(labels, cv2.COLOR_RGB2BGR)
+
+      if overlay == True:
+        alpha = 0.7
+        cv2.addWeighted(new_img, alpha, image_overlay, 1 - alpha,0, image_overlay)
+
+        if output_image_name is not None:
+          cv2.imwrite(output_image_name, image_overlay)
+          print("Processed Image saved successfully in your current working directory.")
+
+        return raw_labels, image_overlay
+
+        
+      else:  
         if output_image_name is not None:
   
           cv2.imwrite(output_image_name, new_img)
