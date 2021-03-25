cf34a7a1f47d2f2c30e09c51d2232da09160bd22
NickiRom <romano.nh@gmail.com>
2021-03-25 10:40:56 -0700

    added option to create copy of image in apply_mask

--- pixellib/instance.py
+++ pixellib/instance.py
@@ -840,16 +840,24 @@ def random_colors(N, bright=True):
     return colors
 
 
-def apply_mask(image, mask, color, alpha=0.5):
+def apply_mask(image, mask, color, alpha=0.5, inplace=True):
     """Apply the given mask to the image.
     """
-    for c in range(3):
-        image[:, :, c] = np.where(mask == 1,
-                                  image[:, :, c] *
-                                  (1 - alpha) + alpha * color[c] * 255,
-                                  image[:, :, c])
-    return image
-
+    if inplace:
+        for c in range(3):
+            image[:, :, c] = np.where(mask == 1,
+                                      image[:, :, c] *
+                                      (1 - alpha) + alpha * color[c] * 255,
+                                      image[:, :, c])
+        return image
+    else:
+        image_copy = np.copy(image, subok=True)
+        for c in range(3):
+            image_copy[:, :, c] = np.where(mask == 1,
+                                      image_copy[:, :, c] *
+                                      (1 - alpha) + alpha * color[c] * 255,
+                                      image_copy[:, :, c])
+        return image_copy
     
 
 
